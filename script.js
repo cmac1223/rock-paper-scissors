943713d6543824f966bb7f17de6e82afb0c6ab66
Chris McClain <chrismac539@gmail.com>
2022-03-26 06:13:49 -0400

    computerPlay func works! random numbers are return

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 // create a functin that takes a max num and console.log a random num up to the max num
 // only returning whole nums
 
-const Rock = 0;
-const Paper = 1;
-const Scissors = 2;
+const Rock = 1;
+const Paper = 2;
+const Scissors = 3;
+
+let computerPlay = (max) => {
+  console.log( Math.ceil(Math.random() * max));
+}
