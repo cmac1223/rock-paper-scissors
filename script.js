1d8826ef63eb8d01fec939a8b0082c62bbc4455f
Chris McClain <chrismac539@gmail.com>
2022-03-27 11:09:42 -0400

    created working function that selects winner of r p s

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ const Scissors = 3;
 let computerPlay = () => {
   let randomNumber = Math.ceil(Math.random() * 3);
   if (randomNumber === 1) {
-    console.log('Rock');
+    // console.log('ROCK');
+    return 'ROCK'
   } else if (randomNumber === 2) {
-    console.log('Paper')
+    // console.log('PAPER')
+    return 'PAPER'
   } else if (randomNumber === 3) {
-    console.log('Scissors')
+    // console.log('SCISSORS')
+    return 'SCISSORS'
   }
 }
 
@@ -24,3 +27,52 @@ let computerPlay = () => {
 // two parameters playerSelection and computerSelection
 // returns string that declares the winner of round
 // make playerSelection case-insensitive
+
+
+let playGame = (playerSelection, computerSelection) => {
+  // debugger;
+  computerSelection = computerPlay();
+  playerSelection2 = playerSelection.toUpperCase()
+
+  // console.log(playerSelection2)
+  // console.log(computerSelection)
+
+  // Case where player select Paper.
+  if (playerSelection2 === 'PAPER' && computerSelection === 'PAPER') {
+    console.log('Tied! please go again.')
+  }
+  if (playerSelection2 === 'PAPER' && computerSelection === 'ROCK') {
+    console.log('You Won! Paper beats Rock.')
+  }
+  if (playerSelection2 === 'PAPER' && computerSelection === 'SCISSORS') {
+    console.log('You Lose! Scissors beats Paper.')
+  }
+
+
+  // Case where player select Rock.
+  if (playerSelection2 === 'ROCK' && computerSelection === 'ROCK') {
+    console.log('Tied! please go again.')
+  }
+  if (playerSelection2 === 'ROCK' && computerSelection === 'PAPER') {
+    console.log('You Lose! Paper beats Rock.')
+  }
+  if (playerSelection2 === 'ROCK' && computerSelection === 'SCISSORS') {
+    console.log('You Won! Rock beats Scissors.')
+  }
+
+
+  // Case where player select Scissors.
+  if (playerSelection2 === 'SCISSORS' && computerSelection === 'ROCK') {
+    console.log('You Lose! Rock beats Scissors.')
+  }
+  if (playerSelection2 === 'SCISSORS' && computerSelection === 'PAPER') {
+    console.log('You Won! Scissors beats Paper.')
+  }
+  if (playerSelection2 === 'SCISSORS' && computerSelection === 'SCISSORS') {
+    console.log('Tied! please go again.')
+  }
+
+
+  
+
+}
